a8c13b9e85069fc0e9e0b8e6f9adbae38c3d664b
Sachin Mahajan <sachinm78@gmail.com>
2019-03-20 14:58:32 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,9 +2,8 @@ def using_push(countries_in_western_africa, next_country)
     updated_array = countries_in_western_africa.push(next_country)
 end
 
-def using_unshift(neighborhoods_in_northwest_brooklyn, 
-        countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
-    next_country = "Niger"
+def using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhood)
+
     updated_array = neighborhoods_in_northwest_brookly.unshift(new_neighborhood)
 end
 
